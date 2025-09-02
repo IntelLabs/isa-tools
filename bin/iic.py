4c4d695ad9c38ff140f3e0d5d0f1d950a3afc3e4
Alastair Reid <alastair.reid@intel.com>
2025-09-02 15:19:45 +0100

    iic: revert iii->asli in runtime path

--- bin/iic.py
+++ bin/iic.py
@@ -256,7 +256,7 @@ def get_c_flags(iii, backend):
         # and the include path that we need is ../_build/install/default/runtime/include
         bindir = os.path.dirname(iii)
         rootdir = os.path.dirname(bindir)
-        path = os.path.join(rootdir, "lib/iii/runtime_include")
+        path = os.path.join(rootdir, "lib/asli/runtime_include")
         c_flags = [f"-I{path}"]
     c_flags.extend(backend_c_flags[backend])
     return c_flags
@@ -279,7 +279,7 @@ def get_ld_flags(iii, backend):
         # and the include path that we need is ../_build/install/default/runtime
         bindir = os.path.dirname(iii)
         rootdir = os.path.dirname(bindir)
-        path = os.path.join(rootdir, "lib/iii/runtime/libASL.a")
+        path = os.path.join(rootdir, "lib/asli/runtime/libASL.a")
         ld_flags = [path]
     if backend == "sc":
         sc_types_dir = os.environ.get('SC_TYPES_DIR')
