ed1ca6faae74eefd2f5d2bde386989de1a138241
Nikolay Kosarev <nikolay.kosarev@intel.com>
2026-01-12 15:24:27 +0100

    Fix description of --ffi-integer argument

--- bin/iic.py
+++ bin/iic.py
@@ -522,7 +522,7 @@ def main() -> int:
     parser.add_argument("--import", dest="imports", help="import this symbol (C generation only)", action='append', default=[])
     parser.add_argument("--line-info", help="insert line directives into C code", action=argparse.BooleanOptionalAction)
     parser.add_argument("--new-ffi", help="use the new FFI", action="store_true", default=False)
-    parser.add_argument("--ffi-integer", help="select type for Integer in new FFI (default: int64_t)", choices=['int', 'int64_t'], default='int')
+    parser.add_argument("--ffi-integer", help="select type for Integer in new FFI (default: int)", choices=['int', 'int64_t'], default='int')
     parser.add_argument("--runtime-checks", help="perform runtime checks (array bounds, etc.)", action=argparse.BooleanOptionalAction)
     parser.add_argument("--split-state", help="split state into multiple structs", action=argparse.BooleanOptionalAction)
     parser.add_argument("--transform-foreign", help="process foreign import/export variable declarations", action=argparse.BooleanOptionalAction)
